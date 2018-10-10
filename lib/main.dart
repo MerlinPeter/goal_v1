e50e90e8632c116eac011ddb1cbf04febb64035b
goal <merlinahila@gmail.com>
2018-10-10 12:04:55 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'loginPage.dart';
+
+void main(){
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+
+     return new MaterialApp(
+       title: 'Goal App Login' ,
+       theme: new ThemeData( primarySwatch: Colors.blue,
+     ),
+     home :new LoginPage()
+     );
+  }
+}
